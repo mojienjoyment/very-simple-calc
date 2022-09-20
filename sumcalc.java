7ca5c0f7c4f7cc33e1a3e625ef4eaa66e77aebc7
Moji <68327458+mojienjoyment@users.noreply.github.com>
2022-09-20 18:05:37 +0430

    first commit

---
+++ sumcalc.java
@@ -0,0 +1,13 @@
+package iran;
+import java.util.Scanner;
+public class calc {
+
+	public static void main(String[] args) {
+		Scanner get = new Scanner(System.in);
+		System.out.println("Please input number a:");
+		int a = get.nextInt();
+		System.out.println("Please input number b:");
+		int b = get.nextInt();
+		System.out.println("sum of a and b is = "+(a+b));
+	}
+}
